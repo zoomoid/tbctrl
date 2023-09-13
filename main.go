0004ab6ef77d65f18436d16e63281ed7c374865c
Alexander Bartolomey <occloxium@gmail.com>
2023-09-13 12:46:24 +0200

    fix!: make controller run in host network
    this requires the user to provide ways of accessing the API server without touching the CNI
    
    for this, provide --k8s-service-host (or $K8S_SERVICE_HOST) and --k8s-service-port (or $K8S_SERVICE_PORT)
    
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net"
 	"os"
 
 	flag "github.com/spf13/pflag"
@@ -39,17 +40,30 @@ func main() {
 	var probeAddr string
 	var enableLeaderElection bool
 
+	var k8sServiceHost string = os.Getenv("K8S_SERVICE_HOST")
+	var k8sServicePort string = os.Getenv("K8S_SERVICE_PORT")
+
 	flag.IntVar(&logLevel, "v", 0, "klog level ranges from -5 (Fatal) to 10 (Verbose)")
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+
+	if k8sServiceHost == "" && k8sServicePort == "" {
+		flag.StringVar(&k8sServiceHost, "k8s-service-host", "localhost", "Hostname to use to connect to the API server to. This is required, because otherwise the controller needs the CNI to be setup (which, it turns out, we cannot ensure.)")
+		flag.StringVar(&k8sServicePort, "k8s-service-port", "6443", "Port to use to connect to the API server to. This is required, because otherwise the controller needs the CNI to be setup (which, it turns out, we cannot ensure.)")
+	}
+
 	flag.Parse()
 
 	l := klog.NewKlogr().V(logLevel)
 	ctrl.SetLogger(l)
 
+	// create a custom client config for connecting to the API server from the host network namespace
+	cfg := ctrl.GetConfigOrDie()
+	cfg.Host = net.JoinHostPort(k8sServiceHost, k8sServicePort)
+
 	l.V(0).Info("Starting controller", "version", Version, "build", Build)
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme: scheme,
 		// MetricsBindAddress:     metricsAddr,
 		// Port:                   9443,
