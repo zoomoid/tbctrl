f3cacce141f1590bb503bb0b6483d28b38b2835f
Alexander Bartolomey <github@alexanderbartolomey.de>
2025-08-29 08:58:04 +0200

    refactor: adjustments for metrics and new controller runtime
    Signed-off-by: Alexander Bartolomey <github@alexanderbartolomey.de>

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
 
 	flag "github.com/spf13/pflag"
@@ -16,11 +18,12 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
-
 	controller "github.com/zoomoid/tbctrl/controllers"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 )
 
 var (
@@ -39,7 +42,7 @@ func init() {
 	viper.BindEnv("metrics_bind_addr")
 	viper.BindEnv("health_probe_bind_addr")
 	viper.BindEnv("enable_leader_election")
-
+	viper.BindEnv("enable_http2")
 	viper.BindEnv("k8s_service_host")
 	viper.BindEnv("k8s_service_port")
 
@@ -51,7 +54,8 @@ func init() {
 	flag.String("metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.String("health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.Bool("enable-leader-election", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-
+	flag.Bool("enable-http2", false, "Enable http/2 for the connection to the API server. http/2 should be disabled due to its vulnerabilities.")
+	flag.Bool("metrics-secure", true, "If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.String("k8s-service-host", "localhost", "Hostname to use to connect to the API server to. This is required, because otherwise the controller needs the CNI to be setup (which, it turns out, we cannot ensure.)")
 	flag.String("k8s-service-port", "6443", "Port to use to connect to the API server to. This is required, because otherwise the controller needs the CNI to be setup (which, it turns out, we cannot ensure.)")
 }
@@ -67,15 +71,60 @@ func main() {
 	cfg := ctrl.GetConfigOrDie()
 	cfg.Host = net.JoinHostPort(viper.GetString("k8s-service-host"), viper.GetString("k8s-service-port"))
 
+	var tlsOpts []func(*tls.Config)
+
+	// Create watchers for metrics and webhooks certificates
+	var metricsCertWatcher *certwatcher.CertWatcher
+
+	metricsServerOptions := metricsserver.Options{
+		BindAddress:    viper.GetString("metrics-bind-address"),
+		SecureServing:  viper.GetBool("metrics-secure"),
+		TLSOpts:        tlsOpts,
+		FilterProvider: filters.WithAuthenticationAndAuthorization,
+	}
+
+	if metricsCertPath := viper.GetString("metrics-cert-path"); len(metricsCertPath) > 0 {
+		metricsCertName := viper.GetString("metrics-cert-name")
+		metricsCertKey := viper.GetString("metrics-cert-key")
+		l.Info("Initializing metrics certificate watcher using provided certificates",
+			"metrics-cert-path", metricsCertPath, "metrics-cert-name", metricsCertName, "metrics-cert-key", metricsCertKey)
+
+		var err error
+		metricsCertWatcher, err = certwatcher.New(
+			filepath.Join(metricsCertPath, metricsCertName),
+			filepath.Join(metricsCertPath, metricsCertKey),
+		)
+		if err != nil {
+			l.Error(err, "to initialize metrics certificate watcher", "error", err)
+			os.Exit(1)
+		}
+
+		metricsServerOptions.TLSOpts = append(metricsServerOptions.TLSOpts, func(config *tls.Config) {
+			config.GetCertificate = metricsCertWatcher.GetCertificate
+		})
+	}
+
+	if viper.GetBool("metrics-secure") {
+		// FilterProvider is used to protect the metrics endpoint with authn/authz.
+		// These configurations ensure that only authorized users and service accounts
+		// can access the metrics endpoint. The RBAC are configured in 'config/rbac/kustomization.yaml'. More info:
+		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/metrics/filters#WithAuthenticationAndAuthorization
+		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
+	}
+
+	disableHTTP2 := func(c *tls.Config) {
+		l.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	if !viper.GetBool("enable-http2") {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
 	l.V(0).Info("Starting controller", "version", Version, "build", Build)
 	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme,
-		// MetricsBindAddress:     metricsAddr,
-		// Port:                   9443,
-		Metrics: server.Options{
-			BindAddress: viper.GetString("metrics-bind-address"),
-		},
-
+		Scheme:                 scheme,
+		Metrics:                metricsServerOptions,
 		HealthProbeBindAddress: viper.GetString("health-probe-bind-address"),
 		LeaderElection:         viper.GetBool("enable-leader-election"),
 		LeaderElectionID:       "tbctrl",
